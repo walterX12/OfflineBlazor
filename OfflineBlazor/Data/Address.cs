6880d8e646fcc5abd04b0962254c2d3e5a400601
Petr Komarek <petr.komarek@global.ntt>
2021-12-17 16:13:28 +0100

    Added Initila offline version

---
+++ OfflineBlazor/Data/Address.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OfflineBlazor.Data
+{
+    public class Address
+    {
+        [Required]
+        public string Street { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string PostCode { get; set; }
+        [Required]
+        public string State { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"{Street} {PostCode} {City} {State}";
+        }
+    }
+}
---
+++ OfflineBlazor/Pages/FillTheForm.razor.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Components;
+using OfflineBlazor.Data;
+
+namespace OfflineBlazor.Pages
+{
+    public partial class FillTheForm: ComponentBase
+    {
+        [Parameter]
+        public Address Address { get; set; }
+
+
+        /// <summary>
+        /// Message juts for demo purpose. This message is to replace proper form processing (e.g. saving to database)
+        /// </summary>
+        private string AfterSubmitMessage { get; set; }
+
+        protected override void OnInitialized()
+        {
+            if (Address == null)
+            {
+                Address = new Address()
+                {
+                    Street = "Lovel Close 3",
+                    City = "Roayl on Themes",
+                    PostCode = "RG97RS",
+                    State = "Kingdom of People",
+                };
+            }
+            base.OnInitialized();
+        }
+
+        private void HandleValidSubmit()
+        {
+            //Logger.LogInformation("HandleValidSubmit called");
+            AfterSubmitMessage = $"Thank you for filling the form. We will send youthe goods to the address:{Address}";
+        }
+    }
+}
